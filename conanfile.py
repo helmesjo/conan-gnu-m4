a386462293bdac0f95e1fa0853525b62c04a0e38
helmesjo <helmesjo@live.com>
2018-07-19 22:41:39 +0200

    Added gpl-3.0 license to package

--- conanfile.py
+++ conanfile.py
@@ -12,26 +12,17 @@ class LibnameConan(ConanFile):
     url = "https://github.com/helmesjo/conan-gnu-m4"
     homepage = "https://www.gnu.org/software/m4/m4.html"
     author = "helmesjo <helmesjo@gmail.com>"
-    # Indicates License type of the packaged library
     license = "GPL"
-
-    # Packages the license for the conanfile.py
     exports = ["LICENSE.md"]
-
-    # Options may need to change depending on the packaged library.
+    
     settings = "os", "arch", "build_type"
     options = {"fPIC": [True, False]}
     default_options = "fPIC=True"
-
-    # Custom attributes for Bincrafters recipe conventions
+    
     source_subfolder = "source_subfolder"
     build_subfolder = "build_subfolder"
-
-    # Use version ranges for dependencies unless there's a reason not to
-    # Update 2/9/18 - Per conan team, ranges are slow to resolve.
-    # So, with libs like zlib, updates are very rare, so we now use static version
-
-
+    license_filename = "gpl-3.0.md"
+    
     requires = ()
 
     def requirements(self):
@@ -47,9 +38,11 @@ class LibnameConan(ConanFile):
         source_url = "http://ftp.gnu.org/gnu/m4/"
         extracted_dir = "m4-{}".format(self.version)
         tools.get("{}/{}.tar.gz".format(source_url, extracted_dir))
-        
         # Rename to "source_subfolder" is a convention to simplify later steps
         os.rename(extracted_dir, self.source_subfolder)
+
+        with tools.chdir(self.source_subfolder):
+            tools.download(url="https://www.gnu.org/licenses/gpl-3.0.md", filename=self.license_filename)
 
     def configure_autotools(self):
         env_build = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
@@ -68,7 +61,7 @@ class LibnameConan(ConanFile):
             env_build.make()
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src=self.source_subfolder)
+        self.copy(pattern=self.license_filename, dst="licenses", src=self.source_subfolder)
         
         with tools.chdir(self.source_subfolder):
             env_build = self.configure_autotools()
